1e1c584e9d5605637f1e719b7fa99941f49903ad
Marvin Cortez <marvin.cortez@painsa.com.gt>
2019-05-16 23:36:14 -0600

    nuevo

--- database/migrations/2001_05_1_051541_create_country_table.php
+++ database/migrations/2001_05_1_051541_create_country_table.php
@@ -13,7 +13,7 @@ class CreateCountryTable extends Migration
      */
     public function up()
     {
-        Schema::create('country', function (Blueprint $table) {
+        Schema::create('country      ', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
             $table->timestamps();
